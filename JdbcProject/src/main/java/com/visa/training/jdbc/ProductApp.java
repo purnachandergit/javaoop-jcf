dfd1ba3d88b52a774c96283e87bb42037428cd3e
root <purnachander.daram@gmail.com>
2019-08-05 07:10:09 +0000

    Day 6 5/8/2019

---
+++ JdbcProject/src/main/java/com/visa/training/jdbc/ProductApp.java
@@ -0,0 +1,20 @@
+package com.visa.training.jdbc;
+
+import java.util.List;
+
+public class ProductApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JdbcProductDAO dao=new JdbcProductDAO();
+		Product test=new Product("test",19999,100);
+		Product saved=dao.save(test);
+		//System.out.println("created product with id:"+saved.getId());
+		System.out.println("Created product with id "+saved.getId());
+        System.out.println("_________________________All products_____________________");
+		List<Product> all=dao.findAll();
+		all.forEach(System.out::println);
+
+	}
+
+}
